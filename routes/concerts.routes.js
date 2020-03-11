9ee5226dfb9b09ee5c12756afa70d09698e98d09
Marcin Dzieszkowski <m.dzieszkowski@gmail.com>
2020-03-11 20:49:38 +0100

    add additional endpoints and their tests

--- routes/concerts.routes.js
+++ routes/concerts.routes.js
@@ -10,4 +10,9 @@ router.post('/concerts', ConcertController.postOne);
 router.put('/concerts/:id', ConcertController.updateOneById);
 router.delete('/concerts/:id', ConcertController.deleteOneById);
 
+router.get('/concerts/performer/:performer', ConcertController.getPerformer);
+router.get('/concerts/genre/:genre', ConcertController.getGenre);
+router.get('/concerts/price/:price_min/:price_max', ConcertController.getPrice);
+router.get('/concerts/day/:day', ConcertController.getDay);
+
 module.exports = router;
--- server.js
+++ server.js
@@ -47,6 +47,7 @@ const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
 
+module.exports = server;
 const io = socket(server);
 
 io.on('connection', socket => {
